5ea18452919dff6ae3d8d42c88bf861158215bd8
Joonaxii <joonaxii@gmail.com>
2026-01-30 22:22:05 +0200

    Fix Party Goer, add simple Locator

--- Assets/Scripts/Interfaces/ILocatable.cs
+++ Assets/Scripts/Interfaces/ILocatable.cs
@@ -7,5 +7,25 @@ public interface ILocatable
     bool IsActive { get; set; }
     Vector3 Position { get; }
     float Radius { get; }
-    int Layer { get; }
+    ObjectKind Kind { get; }
 }
+
+public enum ObjectKind : int
+{
+    Default,
+    PartyGore,
+    Guard,
+    Prop,
+}
+
+[System.Flags]
+public enum ObjectKindMask : int
+{
+    Default = 0x1,
+    Baller = 0x2,
+    Guard = 0x4,
+    Prop = 0x8,
+    
+    All = -1
+}
+
