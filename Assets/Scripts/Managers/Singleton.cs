f215dd5dfb5b8886705bb7c61a5c1317b4860ac1
Joonaxii <joonaxii@gmail.com>
2026-01-30 20:51:22 +0200

    Add Singleton

---
+++ Assets/Scripts/Managers/Singleton.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    public static T Instance => _instance;
+    private static T _instance;
+    
+    protected void Awake()
+    {
+        if (_instance != this && _instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        _instance = this as T;
+        DontDestroyOnLoad(gameObject);
+    }
+}
