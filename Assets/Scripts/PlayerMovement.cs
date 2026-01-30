b0227dc20683d54efbf004da774c79752232c2a2
Hippopotaska <hippopotaska@gmail.com>
2026-01-30 19:39:07 +0200

    Added playermovement, added player to the scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+
+    private Vector3 _velocity;
+    private Vector3 _moveDirection;
+
+    private const float MOVE_LIMITER = 0.7f;
+    
+    void Start()
+    {
+        _moveDirection = Vector3.zero;
+        _velocity = Vector3.zero;
+    }
+    
+    void Update()
+    {
+        _moveDirection.x = Input.GetAxisRaw("Horizontal");
+        _moveDirection.y = Input.GetAxisRaw("Vertical");
+        
+        _velocity = _moveDirection * _movementSpeed;
+        _velocity *= IsDiagonalMovement() ? MOVE_LIMITER : 1f;
+        transform.position += _velocity * Time.deltaTime;
+    }
+
+    private bool IsDiagonalMovement()
+    {
+        return _moveDirection.x != 0 && _moveDirection.y != 0;
+    }
+}
