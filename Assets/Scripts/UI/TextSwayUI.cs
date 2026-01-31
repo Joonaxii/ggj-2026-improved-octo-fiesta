17e702c9998d0ce3f156939f3134082b005cc91f
Hippopotaska <hippopotaska@gmail.com>
2026-01-31 23:18:52 +0200

    Added game result screen

--- Assets/Scripts/UI/TextSwayUI.cs
+++ Assets/Scripts/UI/TextSwayUI.cs
@@ -4,6 +4,7 @@ public class TextSwayUI : MonoBehaviour
 {
     private float _swayOffset;
     private RectTransform _rectTransform;
+    private float _initialZRotation;
 
     private float _swayLimitter = 0.05f;
 
@@ -11,6 +12,7 @@ public class TextSwayUI : MonoBehaviour
     {
         _swayOffset = Random.Range(-10, 10);
         _rectTransform = GetComponent<RectTransform>();
+        _initialZRotation = _rectTransform.localRotation.z;
     }
     
     
@@ -18,7 +20,7 @@ public class TextSwayUI : MonoBehaviour
     {
         var zVal = Mathf.Sin((Time.fixedTime * 2) + _swayOffset);
         var newRotation = _rectTransform.rotation;
-        newRotation.z = zVal * _swayLimitter;
+        newRotation.z = (zVal * _swayLimitter) + _initialZRotation;
         _rectTransform.rotation = newRotation;
     }
 }
